505d80aa882016fdb1cffe5162b79cb25f57c3fc
Jacob Husband <jakehusband2@gmail.com>
2022-09-16 21:01:45 +0000

    swagv9

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 var data = {
-  view: 'home',
-  entries: []
+  monday: [],
+  tuesday: [],
+  wednesday: [],
+  thursday: [],
+  friday: [],
+  saturday: [],
+  sunday: [],
 };
-
 var $daysOfTheWeek = document.querySelector('.days-of-the-week');
+var $addEntryButton = document.querySelector('.add-entry');
+
 $daysOfTheWeek.addEventListener('click', handleClick);
+$addEntryButton.addEventListener('click', showEntryModal);
+
 function handleClick(event) {
   if (event.target.matches('.day-text')) {
     var $homeView = document.querySelector('[data-view = "home"]');
     $homeView.className = 'w-100 hidden';
     var $dayView = document.querySelector('[data-view = "Day"]');
     $dayView.className = 'w-100';
+    $dayView.querySelector('h1').textContent = event.target.textContent;
   }
+}
+
+function showEntryModal(event) {
+
 }
