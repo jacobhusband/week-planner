46199e986e36e72a2f1b37d1093e8be1f59787a7
Jacob Husband <jakehusband2@gmail.com>
2022-09-16 22:58:08 +0000

    swagv14

--- main.js
+++ main.js
@@ -11,10 +11,12 @@ var $daysOfTheWeek = document.querySelector('.days-of-the-week');
 var $addEntryButton = document.querySelector('.add-entry');
 var $form = document.querySelector('form');
 var $h1 = document.querySelectorAll('h1');
+var $tbody = document.querySelector('tbody');
 
 $daysOfTheWeek.addEventListener('click', handleClick);
 $addEntryButton.addEventListener('click', showEntryModal);
 $form.addEventListener('submit', handleFormSubmit);
+window.addEventListener('beforeunload', storeData);
 
 function handleClick(event) {
   if (event.target.matches('.day-text')) {
@@ -23,6 +25,17 @@ function handleClick(event) {
     var $dayView = document.querySelector('[data-view = "Day"]');
     $dayView.className = 'w-100';
     $dayView.querySelector('h1').textContent = event.target.textContent;
+    removeTableData();
+    renderTable(event.target.textContent.toLowerCase());
+  }
+}
+
+function removeTableData() {
+  var tr = $tbody.lastElementChild;
+
+  while (tr) {
+    $tbody.removeChild(tr);
+    tr = $tbody.lastElementChild;
   }
 }
 
@@ -38,13 +51,27 @@ function handleFormSubmit(event) {
   var h1Text = $h1[1].textContent.toLowerCase();
   data[h1Text] = [{ description, time: entryTime }];
   event.target.closest('.entry-modal-container').classList.add('hidden');
+  renderTable(h1Text);
 }
 
 function renderTable(day) {
+  var tr, td;
   var dayData = data[day];
-  for (var i = 0; i < dayData.length; i++) {
-    for (var key in dayData[i]) {
-
+  if (dayData) {
+    for (var i = 0; i < dayData.length; i++) {
+      tr = document.createElement('tr');
+      for (var key in dayData[i]) {
+        td = document.createElement('td');
+        td.textContent = dayData[i][key];
+        tr.prepend(td);
+      }
+      $tbody.appendChild(tr);
     }
   }
 }
+
+function storeData(event) {
+  localStorage.setItem('data', JSON.stringify(data));
+}
+
+data = JSON.parse(localStorage.getItem('data'));
