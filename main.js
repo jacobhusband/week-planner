6116277698709483ad3a84723b9d9850c129038b
Markus Doan-Le <markus.doanle@gmail.com>
2022-09-16 22:39:52 +0000

    swag v13

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var data = {
 var $daysOfTheWeek = document.querySelector('.days-of-the-week');
 var $addEntryButton = document.querySelector('.add-entry');
 var $form = document.querySelector('form');
-var $h1 = document.querySelector('h1');
+var $h1 = document.querySelectorAll('h1');
 
 $daysOfTheWeek.addEventListener('click', handleClick);
 $addEntryButton.addEventListener('click', showEntryModal);
@@ -27,13 +27,24 @@ function handleClick(event) {
 }
 
 function showEntryModal(event) {
-
+  var $modalContainer = document.querySelector('.entry-modal-container');
+  $modalContainer.classList.remove('hidden');
 }
 
 function handleFormSubmit(event) {
   event.preventDefault();
   var description = $form.elements.description.value;
   var entryTime = $form.elements['entry-time'].value;
-  var h1Text = $h1.textContent.toLowerCase();
+  var h1Text = $h1[1].textContent.toLowerCase();
   data[h1Text] = [{ description, time: entryTime }];
+  event.target.closest('.entry-modal-container').classList.add('hidden');
+}
+
+function renderTable(day) {
+  var dayData = data[day];
+  for (var i = 0; i < dayData.length; i++) {
+    for (var key in dayData[i]) {
+
+    }
+  }
 }
