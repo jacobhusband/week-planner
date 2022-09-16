1e9b47d763d870b59010b12ddd484d458451983d
Markus Doan-Le <markus.doanle@gmail.com>
2022-09-16 18:55:38 +0000

    swagv5

---
+++ main.js
@@ -0,0 +1,12 @@
+var data = {
+  view: 'home',
+  entries: []
+};
+
+var $daysOfTheWeek = document.querySelector('.days-of-the-week');
+$daysOfTheWeek.addEventListener('click', handleClick);
+function handleClick(event) {
+  if (event.target.matches('.day-text')) {
+
+  }
+}
