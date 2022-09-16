27118edc01619f6f26d8b542fe0c21945a3c32f0
Markus Doan-Le <markus.doanle@gmail.com>
2022-09-16 20:47:23 +0000

    swag v8

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ var $daysOfTheWeek = document.querySelector('.days-of-the-week');
 $daysOfTheWeek.addEventListener('click', handleClick);
 function handleClick(event) {
   if (event.target.matches('.day-text')) {
-
+    var $homeView = document.querySelector('[data-view = "home"]');
+    $homeView.className = 'w-100 hidden';
+    var $dayView = document.querySelector('[data-view = "Day"]');
+    $dayView.className = 'w-100';
   }
 }
